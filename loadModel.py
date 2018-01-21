d5976a4b72e1615066a73adec3f13e7b3c6eb365
atroudi <atroudi@qf.org.qa>
2018-01-21 10:35:34 +0300

    load Model adaptation with rheem.

--- loadModel.py
+++ loadModel.py
@@ -1,73 +1,70 @@
-import keras
 import numpy
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-
+import sys
 from numpy import loadtxt
-from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn import linear_model
-from sklearn.ensemble import RandomForestRegressor
 import pickle
 import os
 
-#inputFile = loadtxt("planVectorsSGD2-kmeans-simword-opportuneWordcount.txt", comments="#", delimiter=" ", unpack=False)
-inputFile = loadtxt("C:\\Users\\VettaEx1\\.rheem\\mlModelVectors.txt", comments="#", delimiter=" ", unpack=False)
+def main():
 
-#size = 146;
-#start = 13;
-#size = 213
-size=250
-start = 0
-x_test = inputFile[:,0:size]
-y_test = inputFile[start:,size]
-
-# x_train = inputFile[:,0:size]
-# y_train = inputFile[:,size]
-#
-# x_test = inputFile[:,0:size]
-# y_test = inputFile[:,size]
-
-
-
-# load the model from disk
-filename = 'ForestModel.sav'
-regr = pickle.load(open(filename, 'rb'))
-
-
-
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
-#kfold = KFold(n_splits=10, random_state=seed)
-#results = cross_val_score(regr, x_train, y_train, cv=kfold)
-#accuracy_score(prediction,y_train)
-#print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
-prediction = regr.predict(x_test)
-
-for num in range(1, 34):
-    if num % 2 == 0:
-        print("estimated time for " + str(x_test[num][size-2]) + "-" + str(x_test[num][size-1]) + " in java : " + str(
-            prediction[num]) + "(real " + str(y_test[num]) + ")")
+    #inputFile = loadtxt("planVectorsSGD2-kmeans-simword-opportuneWordcount.txt", comments="#", delimiter=" ", unpack=False)
+    if (len(sys.argv)>=2):
+        inputFile = loadtxt(sys.argv[1], comments="#", delimiter=" ", unpack=False)
     else:
-        print("estimated time for " + str(x_test[num][size-2]) + "-" + str(x_test[num][size-1]) + " in spark : " + str(
-            prediction[num]) + "(real " + str(y_test[num]) + ")")
+        inputFile = loadtxt("C:\\Users\\FLVBSLV\\.rheem\\mlModelVectors.txt", comments="#", delimiter=" ", unpack=False)
 
-# print results to text
-saveLocation = "estimates.txt"
-# delete first
-os.remove(saveLocation)
-text_file = open(saveLocation, "w")
-text_file.de
-for num in range(1, prediction.size):
-    text_file.write("%d" % prediction[num])
-    text_file.write("\n")
-text_file.close()
+    #size = 146;
+    #start = 13;
+    #size = 213
+    size=213
+    start = 0
+    x_test = inputFile[:,0:size]
+    y_test = inputFile[start:,size]
 
+    # x_train = inputFile[:,0:size]
+    # y_train = inputFile[:,size]
+    #
+    # x_test = inputFile[:,0:size]
+    # y_test = inputFile[:,size]
+
+
+
+    # load the model from disk
+    filename = 'ForestModel.sav'
+    regr = pickle.load(open(filename, 'rb'))
+
+
+
+    # fix random seed for reproducibility
+    seed = 7
+    numpy.random.seed(seed)
+    #kfold = KFold(n_splits=10, random_state=seed)
+    #results = cross_val_score(regr, x_train, y_train, cv=kfold)
+    #accuracy_score(prediction,y_train)
+    #print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+    prediction = regr.predict(x_test)
+
+    for num in range(1, 34):
+        if num % 2 == 0:
+            print("estimated time for " + str(x_test[num][size-2]) + "-" + str(x_test[num][size-1]) + " in java : " + str(
+                prediction[num]) + "(real " + str(y_test[num]) + ")")
+        else:
+            print("estimated time for " + str(x_test[num][size-2]) + "-" + str(x_test[num][size-1]) + " in spark : " + str(
+                prediction[num]) + "(real " + str(y_test[num]) + ")")
+
+    # print results to text
+    if (len(sys.argv) >= 3):
+        saveLocation = loadtxt(sys.argv[2], comments="#", delimiter=" ", unpack=False)
+    else:
+        saveLocation = "C:\\Users\\FLVBSLV\\.rheem\\estimates.txt"
+
+    # delete first
+    if(os._exists(saveLocation)):
+        os.remove(saveLocation)
+    text_file = open(saveLocation, "w")
+    for num in range(1, prediction.size):
+        text_file.write("%d" % prediction[num])
+        text_file.write("\n")
+    text_file.close()
+
+if __name__ == "__main__":
+   main()
